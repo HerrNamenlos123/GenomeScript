86c984a4d13b817c2a7c4219cedea7b167ebabae
HerrNamenlos123 <herrnamenlos123@gmail.com>
2024-03-14 11:51:21 +0100

    Update luabridge and add hooks

--- src/hooks.cpp
+++ src/hooks.cpp
@@ -7,6 +7,33 @@
 #define MAGIC_ENUM_RANGE_MAX 1024
 #include "magic_enum/magic_enum.hpp"
 
+enum eCWrapper_emfx2Actor::eEMotionType {};
+
+/*class GE_DLLIMPORT eCWrapper_emfx2Motion::eSMotionDesc
+        {
+            public:
+                enum eEMotionOwner;
+
+            public:                                 ~eSMotionDesc( void );
+                                                     eSMotionDesc( eSMotionDesc const & );
+                                                     eSMotionDesc( bCString const & );
+                                                     eSMotionDesc( eCResourceAnimationMotion_PS * );
+                                                     eSMotionDesc( eCResourceDataEntity * );
+                                                     eSMotionDesc( void );
+                eSMotionDesc const &                 operator =  ( eSMotionDesc const & );
+                bCOStream &                          operator >> ( bCOStream & );
+                bCIStream &                          operator << ( bCIStream & );
+                eCResourceAnimationMotion_PS *       GetMotion( void );
+                eCResourceAnimationMotion_PS const * GetMotion( void ) const;
+                bCString const &                     GetMotionFilename( void ) const;   
+                eEMotionOwner                        GetMotionOwner( void ) const;
+                GEBool                               IsValid( void ) const;
+                void                                 SetMotion( bCString const & );
+                void                                 SetMotion( eCResourceAnimationMotion_PS * );
+                void                                 SetMotion( eCResourceDataEntity * );
+                void                                 SetMotionOwner( eEMotionOwner );
+        };*/
+
 namespace GenomeScript {
 
 class MusicHook {
@@ -185,56 +212,98 @@ public:
     }
 };
 
+class eCWrapper_emfx2MotionHook {
+public:
+    bEResult LoadMotion(eCArchiveFile& file) {
+        log::error("LoadMotion");
+        HOOK_ACTION("eCWrapper_emfx2Motion", LoadMotion, bEResult, bEResult::bEResult_Ok, bEResult::bEResult_False, file);
+        return LoadMotion_Base(file);
+    }
+
+    DETOUR_DECLARE_MEMBER(LoadMotion, "Engine.dll", "?LoadMotion@eCWrapper_emfx2Motion@@QAE?AW4bEResult@@AAVeCArchiveFile@@@Z");
+
+    static void detour(bool detach) {
+        DETOUR_EXTERN_MEMBER(eCWrapper_emfx2MotionHook, LoadMotion);
+    }
+
+    static void defineLuaTypes(lua_State* state) {
+        luabridge::getGlobalNamespace(state)
+            .beginClass<eCWrapper_emfx2MotionHook>("eCWrapper_emfx2Motion")
+            .addFunction("PlayMotion", &eCWrapper_emfx2MotionHook::LoadMotion_Base<eCArchiveFile>)
+            .endClass();
+    }
+};
+
+class eCVisualAnimation_PSHook : public eCVisualAnimation_PS {
+public:
+    void PlayMotion(eCWrapper_emfx2Actor::eEMotionType type, eCWrapper_emfx2Motion::eSMotionDesc* motion) {
+        HOOK_ACTION_VOID("eCVisualAnimation_PS", PlayMotion, type, motion);
+        PlayMotion_Base(type, motion);
+    }
+
+    DETOUR_DECLARE_MEMBER(PlayMotion, "Engine.dll", "?PlayMotion@eCVisualAnimation_PS@@QAEXW4eEMotionType@eCWrapper_emfx2Actor@@PAUeSMotionDesc@eCWrapper_emfx2Motion@@@Z");
+
+    static void detour(bool detach) {
+        DETOUR_EXTERN_MEMBER(eCVisualAnimation_PSHook, PlayMotion);
+    }
+
+    static void defineLuaTypes(lua_State* state) {
+        luabridge::getGlobalNamespace(state)
+            .beginClass<eCWrapper_emfx2Actor::eEMotionType>("eEMotionType")
+            .endClass();
+        luabridge::getGlobalNamespace(state)
+            .beginClass<eCWrapper_emfx2Motion::eSMotionDesc>("eSMotionDesc")
+            //.addFunction("GetMotionFilename", &eCWrapper_emfx2Motion::eSMotionDesc::GetMotionFilename)
+            .endClass();
+
+        luabridge::getGlobalNamespace(state)
+            .beginClass<eCVisualAnimation_PSHook>("eCVisualAnimation_PS")
+            .addFunction("PlayMotion", &eCVisualAnimation_PSHook::PlayMotion_Base<eCWrapper_emfx2Actor::eEMotionType, eCWrapper_emfx2Motion::eSMotionDesc*>)
+            .addFunction("GetResourceFilePath", &eCVisualAnimation_PSHook::GetResourceFilePath<>)
+            .endClass();
+    }
+};
+
 void detour(bool detach) {
 
     /*DETOUR_EXTERN_MEMBER(eCVirtualFileHook, Open);
     DETOUR_EXTERN_MEMBER(eCVisualAnimationLoDHook, Read);
     DETOUR_EXTERN_MEMBER(eCVisualAnimation_PSHook, Read);
-    DETOUR_EXTERN_MEMBER(eCVisualAnimation_PSHook, PlayMotion);
     DETOUR_EXTERN_MEMBER(eCVisualAnimationFactoryHook, Read);
-    DETOUR_EXTERN_MEMBER(eCWrapper_emfx2MotionHook, LoadMotion);
     DETOUR_EXTERN_MEMBER(EntityHook, DoDamage);
     DETOUR_EXTERN_MEMBER(EntityHook, GetDisplayName);*/
 
     MusicHook::detour(detach);
     eCApplicationHook::detour(detach);
     gCScriptProcessingUnitHook::detour(detach);
+    //eCVisualAnimation_PSHook::detour(detach);
+    eCWrapper_emfx2MotionHook::detour(detach);
 
-    // DETOUR_EXTERN_MEMBER(eCVisualAnimation_PS, PlayMotion, "Engine.dll",
-    // "?PlayMotion@eCVisualAnimation_PS@@QAEXW4eEMotionType@eCWrapper_emfx2Actor@@PAUeSMotionDesc@eCWrapper_emfx2Motion@@@Z");
-    // DETOUR_EXTERN_MEMBER(eCVisualAnimationLoD, Read, "Engine.dll",
-    // "?Read@eCVisualAnimationLoD@@UAE?AW4bEResult@@AAVbCIStream@@@Z"); DETOUR_EXTERN_MEMBER(eCVisualAnimation_PS,
-    // Read, "Engine.dll", "?Read@eCVisualAnimation_PS@@UAE?AW4bEResult@@AAVbCIStream@@@Z");
-    // DETOUR_EXTERN_MEMBER(eCVirtualFile, Open, "Engine.dll",
-    // "?Open@eCVirtualFile@@UAE_NABVbCString@@W4bEFileCreationMode@@E@Z");
+    // DETOUR_EXTERN_MEMBER(eCVisualAnimationLoD, Read, "Engine.dll", "?Read@eCVisualAnimationLoD@@UAE?AW4bEResult@@AAVbCIStream@@@Z"); 
+    // DETOUR_EXTERN_MEMBER(eCVisualAnimation_PS, Read, "Engine.dll", "?Read@eCVisualAnimation_PS@@UAE?AW4bEResult@@AAVbCIStream@@@Z");
+    // DETOUR_EXTERN_MEMBER(eCVirtualFile, Open, "Engine.dll", "?Open@eCVirtualFile@@UAE_NABVbCString@@W4bEFileCreationMode@@E@Z");
 
-    // DETOUR_EXTERN_MEMBER(bCFile, Open, "SharedBase.dll",
-    // "?Open@bCFile@@UAE_NABVbCString@@W4bEFileCreationMode@@@Z"); DETOUR_EXTERN_MEMBER(eCVisualAnimationFactory,
-    // GetMainActorFileName, "Engine.dll", "?GetMainActorFileName@eCVisualAnimationFactory@@QBEABVbCString@@XZ");
-    // DETOUR_EXTERN_MEMBER(eCVisualAnimationFactory, SetMainActorFileName, "Engine.dll",
-    // "?SetMainActorFileName@eCVisualAnimationFactory@@QAEXABVbCString@@@Z");
-    // DETOUR_EXTERN_MEMBER(eCVisualAnimationFactory, Read, "Engine.dll",
-    // "?Read@eCVisualAnimationFactory@@QAE?AW4bEResult@@AAVbCIStream@@@Z"); DETOUR_EXTERN_MEMBER(eCApplication,
-    // GetInstance, "Engine.dll", "?GetInstance@eCApplication@@SGAAV1@XZ"); DETOUR_EXTERN_MEMBER(eCApplication,
-    // GetCurrentCamera, "Engine.dll", "?GetCurrentCamera@eCApplication@@QAEAAVeCCameraBase@@XZ");
+    // DETOUR_EXTERN_MEMBER(bCFile, Open, "SharedBase.dll", "?Open@bCFile@@UAE_NABVbCString@@W4bEFileCreationMode@@@Z"); 
+    // DETOUR_EXTERN_MEMBER(eCVisualAnimationFactory, GetMainActorFileName, "Engine.dll", "?GetMainActorFileName@eCVisualAnimationFactory@@QBEABVbCString@@XZ");
+    // DETOUR_EXTERN_MEMBER(eCVisualAnimationFactory, SetMainActorFileName, "Engine.dll", "?SetMainActorFileName@eCVisualAnimationFactory@@QAEXABVbCString@@@Z");
+    // DETOUR_EXTERN_MEMBER(eCVisualAnimationFactory, Read, "Engine.dll", "?Read@eCVisualAnimationFactory@@QAE?AW4bEResult@@AAVbCIStream@@@Z"); 
+    // DETOUR_EXTERN_MEMBER(eCApplication, GetInstance, "Engine.dll", "?GetInstance@eCApplication@@SGAAV1@XZ"); 
+    // DETOUR_EXTERN_MEMBER(eCApplication, GetCurrentCamera, "Engine.dll", "?GetCurrentCamera@eCApplication@@QAEAAVeCCameraBase@@XZ");
     // DETOUR_EXTERN_MEMBER(eCModuleAdmin, Process, "Engine.dll", "?Process@eCModuleAdmin@@QAEXXZ");
     // DETOUR_EXTERN_MEMBER(eCModuleAdmin, PostProcess, "Engine.dll", "?PostProcess@eCModuleAdmin@@QAEXXZ");
     // DETOUR_EXTERN_MEMBER(eCEntityAdmin, Process, "Engine.dll", "?Process@eCEntityAdmin@@QAEXXZ");
-    // DETOUR_EXTERN_MEMBER(eCEntityAdmin, EnableProcessing, "Engine.dll",
-    // "?EnableProcessing@eCEntityAdmin@@QAEX_N@Z"); DETOUR_EXTERN_MEMBER(eCEntityAdmin, UpdateProcessingRangeEntity,
-    // "Engine.dll",
-    // "?UpdateProcessingRangeEntity@eCEntityAdmin@@QAEXPAVeCEntity@@AAV?$bTSmallArray@PAVeCEntity@@@@1@Z");
+    // DETOUR_EXTERN_MEMBER(eCEntityAdmin, EnableProcessing, "Engine.dll", "?EnableProcessing@eCEntityAdmin@@QAEX_N@Z"); 
+    // DETOUR_EXTERN_MEMBER(eCEntityAdmin, UpdateProcessingRangeEntity, "Engine.dll", "?UpdateProcessingRangeEntity@eCEntityAdmin@@QAEXPAVeCEntity@@AAV?$bTSmallArray@PAVeCEntity@@@@1@Z");
     // DETOUR_EXTERN_MEMBER(eCEntity, EnterProcessingRange, "Engine.dll", "?EnterProcessingRange@eCEntity@@IAEX_N@Z");
     // DETOUR_EXTERN_MEMBER(eCEntity, ExitProcessingRange, "Engine.dll", "?ExitProcessingRange@eCEntity@@IAEX_N@Z");
     // DETOUR_EXTERN_MEMBER(eCEntity, PreProcess, "Engine.dll", "?PreProcess@eCEntity@@QAEX_N@Z");
     // DETOUR_EXTERN_MEMBER(eCEntity, Process, "Engine.dll", "?Process@eCEntity@@QAEX_N@Z");
     // DETOUR_EXTERN_MEMBER(eCEntity, PostProcess, "Engine.dll", "?PostProcess@eCEntity@@QAEX_N@Z");
-    // DETOUR_EXTERN_MEMBER(eCEntity, HasProcessingRangeEntered, "Engine.dll",
-    // "?HasProcessingRangeEntered@eCEntity@@QBE_NXZ"); DETOUR_EXTERN_MEMBER(eCEntity, IsEnabled, "Engine.dll",
-    // "?IsEnabled@eCEntity@@QBE_NXZ"); DETOUR_EXTERN_MEMBER(eCPVSPrefetcher3, GetNonDeactivationCell, "Engine.dll",
-    // "?GetNonDeactivationCell@eCPVSPrefetcher3@@QAEPAVeCPVSCellItem@@XZ"); DETOUR_EXTERN_MEMBER(bCTimer,
-    // GetTimeStamp, "SharedBase.dll", "?GetTimeStamp@bCTimer@@SGKXZ"); DETOUR_EXTERN_MEMBER(bCMemoryAdmin,
-    // GetInstance, "SharedBase.dll", "?GetInstance@bCMemoryAdmin@@SGAAV1@XZ");
+    // DETOUR_EXTERN_MEMBER(eCEntity, HasProcessingRangeEntered, "Engine.dll", "?HasProcessingRangeEntered@eCEntity@@QBE_NXZ"); 
+    // DETOUR_EXTERN_MEMBER(eCEntity, IsEnabled, "Engine.dll", "?IsEnabled@eCEntity@@QBE_NXZ"); 
+    // DETOUR_EXTERN_MEMBER(eCPVSPrefetcher3, GetNonDeactivationCell, "Engine.dll", "?GetNonDeactivationCell@eCPVSPrefetcher3@@QAEPAVeCPVSCellItem@@XZ"); 
+    // DETOUR_EXTERN_MEMBER(bCTimer, GetTimeStamp, "SharedBase.dll", "?GetTimeStamp@bCTimer@@SGKXZ"); 
+    // DETOUR_EXTERN_MEMBER(bCMemoryAdmin, GetInstance, "SharedBase.dll", "?GetInstance@bCMemoryAdmin@@SGAAV1@XZ");
 }
 
 void defineLuaTypes(lua_State* state) {
@@ -246,6 +315,23 @@ void defineLuaTypes(lua_State* state) {
             bcstr->SetText(str.c_str());
             return bcstr;
         })
+        .addFunction("GetText", &bCString::GetText)
+        .endClass();
+    
+    luabridge::getGlobalNamespace(state)
+        .beginClass<bCAnimationResourceString>("bCAnimationResourceString")
+        .addConstructor([](void* ptr, const std::string& str) {
+            bCAnimationResourceString* bcstr = new(ptr) bCAnimationResourceString();
+            bcstr->SetText(str.c_str());
+            return bcstr;
+        })
+        .addFunction("GetText", &bCAnimationResourceString::GetText)
+        .endClass();
+    
+    luabridge::getGlobalNamespace(state)
+        .beginClass<eCArchiveFile>("eCArchiveFile")
+        .addProperty("m_strFileName", &eCArchiveFile::m_strFileName)
+        .addProperty("m_bOpenForWrite", &eCArchiveFile::m_bOpenForWrite)
         .endClass();
 
     DEFINE_LUA_ENUM(gEAction, gEAction_None);
@@ -259,6 +345,8 @@ void defineLuaTypes(lua_State* state) {
     MusicHook::defineLuaTypes(state);
     eCApplicationHook::defineLuaTypes(state);
     gCScriptProcessingUnitHook::defineLuaTypes(state);
+    //eCVisualAnimation_PSHook::defineLuaTypes(state);
+    eCWrapper_emfx2MotionHook::defineLuaTypes(state);
 }
 
 } // namespace GenomeScript
